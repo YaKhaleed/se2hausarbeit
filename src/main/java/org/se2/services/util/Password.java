a3502f99416249b715293b76d78c3343e065234a
Zgjim Morina <zgjim.morina@hotmail.de>
2020-08-25 03:20:28 +0200

    exceptions + dbverbindung 2

---
+++ src/main/java/org/se2/services/util/Password.java
@@ -0,0 +1,13 @@
+package org.se2.services.util;
+
+public class Password {
+
+    private static final String PASSWORT = "qthi2s";
+
+
+    public static String getPasswort() {
+        return PASSWORT;
+    }
+
+}
+
---
+++ src/main/java/org/se2/services/util/Views.java
@@ -0,0 +1,22 @@
+package org.se2.services.util;
+
+public class Views {
+    public static final String MAIN = "suche";
+    public static final String LOGIN = "login";
+    public static final String REGISTER = "register";
+    public static final String REGISTERFUERVERTRIEBLER = "register_vertriebler";
+    public static final String REGISTERFUERKUNDE = "register_kunde";
+    public static final String PASSWORTVERGESSEN = "reset";
+    public static final String STARTSEITE = "startseite";
+    public static final String REGWEITERS = "richtenkontovertriebler";
+    public static final String REGWEITERA = "richtenkontovertriebler";
+    public static final String DASHBOARDA = "vertriebler/dashboard";
+    public static final String DASHBOARDS = "kunde/dashboard";
+    public static final String AUTOANZEIGE = "suche/autoanzeige/jobtitel/";
+    public static final String AUTOANZEIGEERSTELLEN = "vertriebler/autoanzeige";
+
+    private Views() {
+    }
+
+}
+
